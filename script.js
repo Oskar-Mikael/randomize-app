4fcb90e38005e0bfaac48fa70df05c064efdeefe
Oskar-Mikael <oskar.m.bostrom@gmail.com>
2023-01-06 09:42:18 +0100

    Selecting number of items

--- script.js
+++ script.js
@@ -30,43 +30,49 @@ $(document).on('click', 'img.delete', function () {
     let deletedItem = $(this).closest('li')
     let itemValue = $(this).closest('li').text()
     let index = items.indexOf(itemValue);
-    if(index != -1) {
+    if (index != -1) {
         items.splice(index, 1);
     }
     deletedItem.remove();
     console.log(itemValue)
 });
 
-
-
-
 function addItem() {
     if (input.val().length < 1) {
         $('#inputError').html("Add an item please");
-    } else {
-        items.push($(input).val());
-        let ul = $('#itemlist');
-        let li = document.createElement('li');
-        let img = document.createElement('img') 
-        li.append($(input).val());
-        ul.append(li);
-        li.append(img);
-        $(img).addClass('delete')
-        $(img).attr('src', 'close.png');
-        $(input).css('border', '1px solid black');
-        $(input).val('');
-        $('#inputError').html("")
+        return
     }
+    items.push($(input).val());
+    let ul = $('#itemlist');
+    let li = document.createElement('li');
+    let img = document.createElement('img')
+    li.append($(input).val());
+    ul.append(li);
+    li.append(img);
+    $(img).addClass('delete')
+    $(img).attr('src', 'close.png');
+    $(input).css('border', '1px solid black');
+    $(input).val('');
+    $('#inputError').html("")
 }
 
 function displayResult() {
     if (items.length < 1) {
         $('#result').html('Error: Add atleast 1 item')
         $(input).css('border', '2px solid red');
-    } else {
-        result = items[Math.floor(Math.random() * items.length)];
-        $('#result').html(result)
-        $('#result').hide().fadeIn(1000);
+        return;
     }
-}
 
+    let selectNumber = $('#selectNumber').val()
+    const shuffled = items.sort(() => 0.5 - Math.random());
+    result = shuffled.slice(0, selectNumber)
+
+    let resultText = '';
+    result.forEach(function (item) {
+        resultText += item + ', ';
+    });
+
+    $('#result').html(resultText.slice(0, -2))
+    $('#result').hide().fadeIn(1000);
+
+}
